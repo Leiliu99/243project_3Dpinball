1a74e77d193530efcf0ee8eb585171c07f96e00c
Leiliu99 <liulei.liu@mail.utoronto.ca>
2020-03-28 23:07:56 -0400

    initialized the project

---
+++ cmake-build-debug/CMakeFiles/243_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/243_project.dir/main.c.o"
+  "243_project.pdb"
+  "243_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/243_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//this is for 243 final project
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
