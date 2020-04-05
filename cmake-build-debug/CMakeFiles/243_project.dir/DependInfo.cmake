5202ad80a603cc32a2281fa0ca3dac0b44008c16
lilian-z <lilianzhu2000@gmail.com>
2020-04-05 11:03:50 -0400

    debug for reflection of the ball

--- cmake-build-debug/CMakeFiles/243_project.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/243_project.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/liulei/Desktop/243 project/main.c" "/Users/liulei/Desktop/243 project/cmake-build-debug/CMakeFiles/243_project.dir/main.c.o"
+  "/Users/lilianzhu/Documents/GitHub/243project_3Dpinball/main.c" "/Users/lilianzhu/Documents/GitHub/243project_3Dpinball/cmake-build-debug/CMakeFiles/243_project.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/liulei/Desktop/243 project
+# Install script for directory: /Users/lilianzhu/Documents/GitHub/243project_3Dpinball
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/liulei/Desktop/243 project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/lilianzhu/Documents/GitHub/243project_3Dpinball/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
